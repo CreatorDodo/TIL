32e0a4d9bf8122b207e4df0395a3b33a6f7b00b8
Kim Do Young <difbfl4750@gmail.com>
2025-09-23 23:27:54 +0900

    Next.js 빌드 이슈 해결

--- blog/next.config.ts
+++ blog/next.config.ts
@@ -3,6 +3,11 @@ import { withContentlayer } from "next-contentlayer2";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     mdxRs: true,
   },
